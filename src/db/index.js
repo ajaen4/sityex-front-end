16cbad1f6168c1b03fef5b501a67bf3dca333568
ajaen4 <a.jaenrev@gmail.com>
2023-09-20 17:44:29 +0200

    Enable firebase analytics

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, Timestamp } from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics"
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -8,9 +9,12 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 
 const app = initializeApp(config)
 const db = getFirestore(app)
+const analytics = getAnalytics(app)
 
 export { db as default, Timestamp }
