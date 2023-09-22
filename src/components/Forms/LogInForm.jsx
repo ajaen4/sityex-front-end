ba9b8e3595768f802266a16589d4ab24ef9f2eac
ajaen4 <a.jaenrev@gmail.com>
2023-09-22 09:03:37 +0200

    Simplify login structure

--- src/components/Forms/LogInForm.jsx
+++ src/components/Forms/LogInForm.jsx
@@ -1,79 +1,215 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
+
+import { useForm } from "react-hook-form";
 import { useTheme } from "@mui/material/styles";
-import { Link } from "react-router-dom";
+import {
+  Divider,
+  Grid,
+  Typography,
+  Button,
+  Box,
+  useMediaQuery,
+  TextField,
+  FormHelperText,
+  FormControl,
+  Link,
+} from "@mui/material";
 
-import LogInFormBase from "./LogInFormBase";
-import { Grid, Stack, Typography, useMediaQuery } from "@mui/material";
+import { logInUser } from "actions";
 
-import AuthCardWrapper from "../Cards/AuthCardWrapper";
+import StandarModal from "components/Modals/StandarModal";
+import CenteredLoadingSpinner from "components/Spinner/CenteredLoadingSpinner";
+
+import Google from "assets/img/icons/social-google.svg";
+
+import * as ROUTES_PATHS from "routes/paths";
 
 const LogInForm = ({ dispatch }) => {
   const theme = useTheme();
   const matchDownSM = useMediaQuery(theme.breakpoints.down("md"));
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const [isFetching, setIsFetching] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const signInUser = (data) => {
+    setIsFetching(true);
+    dispatch(logInUser(data)).then(
+      (user) => {},
+      (errorMessage) => {
+        setErrorMessage(errorMessage);
+        setIsFetching(false);
+        reset();
+      },
+    );
+  };
+  const googleHandler = async () => {
+    console.log("Login");
+  };
+
+  if (isFetching) return <CenteredLoadingSpinner />;
+
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="flex-end"
-      sx={{ minHeight: "100vh", backgroundColor: theme.palette.primary.light }}
-    >
+    <>
       <Grid item xs={12}>
-        <Grid
-          container
-          justifyContent="center"
-          alignItems="center"
-          sx={{ minHeight: "calc(100vh - 68px)" }}
-        >
-          <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
-            <AuthCardWrapper>
-              <Grid
-                container
-                spacing={2}
-                alignItems="center"
-                justifyContent="center"
+        <Grid container direction="column" justifyContent="center" spacing={2}>
+          <Grid item xs={12}>
+            <Button
+              disableElevation
+              fullWidth
+              onClick={googleHandler}
+              size="large"
+              variant="outlined"
+              sx={{
+                color: "grey.700",
+                backgroundColor: theme.palette.grey[50],
+                borderColor: theme.palette.grey[100],
+              }}
+            >
+              <Box sx={{ mr: { xs: 1, sm: 2, width: 20 } }}>
+                <img
+                  src={Google}
+                  alt="google"
+                  width={16}
+                  height={16}
+                  style={{ marginRight: matchDownSM ? 8 : 16 }}
+                />
+              </Box>
+              Sign in with Google
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                alignItems: "center",
+                display: "flex",
+              }}
+            >
+              <Divider sx={{ flexGrow: 1 }} orientation="horizontal" />
+
+              <Button
+                variant="outlined"
+                sx={{
+                  cursor: "unset",
+                  m: 2,
+                  py: 0.5,
+                  px: 7,
+                  borderColor: `${theme.palette.grey[100]} !important`,
+                  color: `${theme.palette.grey[900]}!important`,
+                  fontWeight: 500,
+                  borderRadius: "14px",
+                }}
+                disableRipple
+                disabled
               >
-                <Grid item sx={{ mb: 3 }}>
-                  <Link to="#"></Link>
-                </Grid>
-                <Grid item xs={12}>
-                  <Grid
-                    container
-                    direction={matchDownSM ? "column-reverse" : "row"}
-                    alignItems="center"
-                    justifyContent="center"
-                  >
-                    <Grid item>
-                      <Stack
-                        alignItems="center"
-                        justifyContent="center"
-                        spacing={1}
-                      >
-                        <Typography
-                          color={theme.palette.secondary.main}
-                          gutterBottom
-                          variant={matchDownSM ? "h3" : "h2"}
-                        >
-                          Hi, Welcome Back
-                        </Typography>
-                        <Typography
-                          variant="caption"
-                          fontSize="16px"
-                          textAlign={matchDownSM ? "center" : "inherit"}
-                        >
-                          Enter your credentials to continue
-                        </Typography>
-                      </Stack>
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <LogInFormBase dispatch={dispatch} />
-              </Grid>
-            </AuthCardWrapper>
+                OR
+              </Button>
+
+              <Divider sx={{ flexGrow: 1 }} orientation="horizontal" />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            container
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="subtitle1">
+              Sign in with Email address
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit(signInUser)}
+              noValidate
+              sx={{
+                mt: 1,
+                width: "83%",
+              }}
+            >
+              <FormControl fullWidth error={Boolean(errors.email)}>
+                <TextField
+                  required
+                  fullWidth
+                  label="Email Address"
+                  placeholder="Email..."
+                  type="email"
+                  autoFocus
+                  error={errors.email !== undefined}
+                  {...register("email", {
+                    required: "You must provide your email",
+                    pattern: {
+                      value:
+                        /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                      message: "The email doesn't have a valid format",
+                    },
+                  })}
+                />
+                <FormHelperText style={{ minHeight: "30px" }}>
+                  {errors.email?.message}
+                </FormHelperText>
+              </FormControl>
+              <FormControl fullWidth error={Boolean(errors.password)}>
+                <TextField
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  placeholder="Password..."
+                  {...register("password", {
+                    required: "The password is required",
+                    minLength: {
+                      value: 8,
+                      message: "The password must have at least 8 characters",
+                    },
+                  })}
+                />
+                <FormHelperText style={{ minHeight: "30px" }}>
+                  {errors.password?.message}
+                </FormHelperText>
+              </FormControl>
+              <Button
+                disabled={Object.keys(errors).length !== 0}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mb: 2 }}
+              >
+                Log In
+              </Button>
+            </Box>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+      <Grid item xs={12}>
+        <Divider />
+      </Grid>
+      <Grid item xs={12}>
+        <Grid item container direction="column" alignItems="center" xs={12}>
+          <Link
+            href={ROUTES_PATHS.SIGN_UP}
+            variant="subtitle1"
+            sx={{ textDecoration: "none" }}
+          >
+            Don&apos;t have an account?
+          </Link>
+        </Grid>
+      </Grid>
+      {errorMessage !== null && (
+        <StandarModal
+          color="error"
+          style={{
+            marginTop: "15px",
+          }}
+          title={"Incorrect authentication. "}
+          message={errorMessage.message}
+        />
+      )}
+    </>
   );
 };
 
--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,11 +1,85 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { useTheme } from "@mui/material/styles";
+import {
+  Grid,
+  Stack,
+  Typography,
+  Container,
+  Box,
+  Card,
+} from "@mui/material";
 
 import { withoutAuth } from "session";
 
 import LogInForm from "components/Forms/LogInForm.jsx";
 
-const LogInPageBase = () => {
-  return <LogInForm />
+const LogInPage = () => {
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ backgroundColor: theme.palette.primary.light }}>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "calc(100vh - 68px)",
+        }}
+      > 
+        <Card
+          sx={{
+            maxWidth: { xs: 400, lg: 475 },
+            margin: { xs: 2.5, md: 3 },
+            border: "1px solid",
+            borderColor: theme.palette.primary[200] + 25,
+            ":hover": {
+              boxShadow: "inherit",
+            },
+            p: { xs: 2, sm: 3, xl: 5 }
+          }}
+        >
+          <Grid
+            container
+            spacing={2}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Grid item sx={{ mb: 3 }}>
+              <Link to="#"></Link>
+            </Grid>
+            <Grid item xs={12}>
+              <Container
+                sx={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Stack alignItems="center" justifyContent="center" spacing={1}>
+                  <Typography
+                    color={theme.palette.secondary.main}
+                    gutterBottom
+                    variant="h3"
+                  >
+                    Hi, Welcome Back
+                  </Typography>
+                  <Typography
+                    variant="caption"
+                    fontSize="16px"
+                    textAlign="center"
+                  >
+                    Enter your credentials to continue
+                  </Typography>
+                </Stack>
+              </Container>
+            </Grid>
+            <LogInForm />
+          </Grid>
+        </Card>
+      </Container>
+    </Box>
+  );
 };
 
-export default withoutAuth(LogInPageBase);
+export default withoutAuth(LogInPage);
