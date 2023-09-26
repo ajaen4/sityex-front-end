9d7454ec8d931af08255b092e230d0a346f38a29
ajaen4 <a.jaenrev@gmail.com>
2023-09-26 10:39:43 +0200

    New version of destinations page, now readable

--- src/pages/DestinationPage.jsx
+++ src/pages/DestinationPage.jsx
@@ -2,16 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { connect } from "react-redux";
 
-import { useTheme } from '@mui/material/styles';
 
 import { withAuth } from "session";
 import { prettyCity } from "helpers/usefulFunctions";
 import { fetchCity, getReviews } from "actions";
 
-import { Box, Typography, Grid, Card, CardContent, Avatar } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import SingleDataCard from "cards/SingleDataCard";
+import RecommendationsMap from 'components/Maps/RecommendationsMap'
 
 import CenteredLoadingSpinner from "components/Spinner/CenteredLoadingSpinner";
-import TableChartOutlinedIcon from '@mui/icons-material/TableChartOutlined';
+
+import GroupsIcon from '@mui/icons-material/Groups';
+import GradingIcon from '@mui/icons-material/Grading';
+import PlaceIcon from '@mui/icons-material/Place';
 
 const DestinationPage = ({
   selectedCity,
@@ -21,38 +25,10 @@ const DestinationPage = ({
 }) => {
   const [reviews, setreviews] = useState([]);
   const { location } = useParams();
-  const theme = useTheme();
 
   useEffect(() => {
     document.title = "Destination Page";
   }, []);
-
-  const cardStyle = {
-    backgroundColor: theme.palette.primary.dark,
-    color: theme.palette.primary.light,
-    overflow: 'hidden',
-    position: 'relative',
-    '&:after': {
-      content: '""',
-      position: 'absolute',
-      width: 210,
-      height: 210,
-      background: `linear-gradient(210.04deg, ${theme.palette.primary[200]} -50.94%, rgba(144, 202, 249, 0) 83.49%)`,
-      borderRadius: '50%',
-      top: -30,
-      right: -180
-    },
-    '&:before': {
-      content: '""',
-      position: 'absolute',
-      width: 210,
-      height: 210,
-      background: `linear-gradient(140.9deg, ${theme.palette.primary[200]} -14.02%, rgba(144, 202, 249, 0) 77.58%)`,
-      borderRadius: '50%',
-      top: -160,
-      right: -130
-    }
-  }
 
   useEffect(() => {
     dispatch(fetchCity(prettyCity(location)));
@@ -67,87 +43,18 @@ const DestinationPage = ({
 
   return (
     <Box>
-      <Grid container spacing={1} sx={{py: 1}}>
-        <Grid item xs={12} md={4}>
-          <Card sx={{...cardStyle, padding: 1}}>
-            <Grid container>
-              <Grid item xs={3} sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <Avatar
-                variant="rounded"
-                sx={{
-                  ...theme.typography.commonAvatar,
-                  ...theme.typography.largeAvatar,
-                  backgroundColor: theme.palette.primary[800],
-                  color: '#fff',
-                }}
-                >
-                  <TableChartOutlinedIcon fontSize="inherit" />
-                </Avatar>
-              </Grid>
-              <Grid item xs={9}>
-              <Typography sx={{ fontSize: 14, color: theme.palette.grey[50] }} color="text.secondary" gutterBottom>
-                People in community
-              </Typography>
-              <Typography variant="body2" sx={{ fontSize: 30, color: theme.palette.grey[50] }}>
-                300
-              </Typography>
-              </Grid>
-            </Grid>
-          </Card>
+      <Grid container spacing={2} sx={{py: 1}} justifyContent="center">
+        <Grid item xs={12} md={3}>
+          <SingleDataCard title="Users" number="553" icon={<GroupsIcon/>}/>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Card sx={{...cardStyle, padding: 1}}>
-            <Grid container>
-              <Grid item xs={3} sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <Avatar
-                variant="rounded"
-                sx={{
-                  ...theme.typography.commonAvatar,
-                  ...theme.typography.largeAvatar,
-                  backgroundColor: theme.palette.primary[800],
-                  color: '#fff',
-                }}
-                >
-                  <TableChartOutlinedIcon fontSize="inherit" />
-                </Avatar>
-              </Grid>
-              <Grid item xs={9}>
-              <Typography sx={{ fontSize: 14, color: theme.palette.grey[50] }} color="text.secondary" gutterBottom>
-                People in community
-              </Typography>
-              <Typography variant="body2" sx={{ fontSize: 30, color: theme.palette.grey[50] }}>
-                300
-              </Typography>
-              </Grid>
-            </Grid>
-          </Card>
+        <Grid item xs={12} md={3} >
+        <SingleDataCard title="Reviews" number="345" icon={<GradingIcon/>}/>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Card sx={{...cardStyle, padding: 1}}>
-            <Grid container>
-              <Grid item xs={3} sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <Avatar
-                variant="rounded"
-                sx={{
-                  ...theme.typography.commonAvatar,
-                  ...theme.typography.largeAvatar,
-                  backgroundColor: theme.palette.primary[800],
-                  color: '#fff',
-                }}
-                >
-                  <TableChartOutlinedIcon fontSize="inherit" />
-                </Avatar>
-              </Grid>
-              <Grid item xs={9}>
-              <Typography sx={{ fontSize: 14, color: theme.palette.grey[50] }} color="text.secondary" gutterBottom>
-                People in community
-              </Typography>
-              <Typography variant="body2" sx={{ fontSize: 30, color: theme.palette.grey[50] }}>
-                300
-              </Typography>
-              </Grid>
-            </Grid>
-          </Card>
+        <Grid item xs={12} md={3}>
+        <SingleDataCard title="Recommended places" number="128" icon={<PlaceIcon/>}/>
+        </Grid>
+        <Grid item xs={12}>
+          <RecommendationsMap selectedCity={selectedCity} />
         </Grid>
       </Grid>
     </Box>
