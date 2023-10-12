c3d2f52ece2eb3ee878b31c7412d8ffd90a1b914
ajaen4 <a.jaenrev@gmail.com>
2023-10-12 15:04:51 +0200

    Fix maps heights

--- src/components/Maps/DestinationsMap.jsx
+++ src/components/Maps/DestinationsMap.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-import { MapContainer, TileLayer } from "react-leaflet";
-import "react-leaflet-fullscreen/styles.css";
-import { FullscreenControl } from "react-leaflet-fullscreen";
+import { MapContainer, TileLayer, ZoomControl } from "react-leaflet";
 import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import { Box } from "@mui/material";
 
 import CitiesMarkerCluster from "components/Maps/CitiesMarkerCluster";
 
@@ -15,18 +14,31 @@ const MAP_STYLE = process.env.REACT_APP_MAPS_STYLE;
 
 function DestinationsMap({ citiesIndex }) {
   return (
-    <MapContainer
-      center={[CENTER["lat"], CENTER["lng"]]}
-      zoom={4}
-      style={{ height: "91.8vh" }}
+    <Box
+      sx={{
+        height: {
+          xs: "86vh",
+          md: "82.5vh",
+          lg: "91.5vh",
+          xl: "93vh",
+        },
+        width: "100%",
+      }}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url={`${MAP_STYLE}${TOKEN}`}
-      />
-      <CitiesMarkerCluster citiesIndex={citiesIndex} />
-      <FullscreenControl position="topright" />
-    </MapContainer>
+      <MapContainer
+        center={[CENTER["lat"], CENTER["lng"]]}
+        zoom={4}
+        style={{ height: "100%", width: "100%" }}
+        zoomControl={false}
+      >
+        <ZoomControl position="topright" />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={`${MAP_STYLE}${TOKEN}`}
+        />
+        <CitiesMarkerCluster citiesIndex={citiesIndex} />
+      </MapContainer>
+    </Box>
   );
 }
 
