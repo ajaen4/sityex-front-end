2e787cea17c760fb88e1b91eeaed3cd02dca18f2
ajaen4 <a.jaenrev@gmail.com>
2024-01-30 11:05:14 +0100

    Add EntreTramites explanation into separate component

--- src/app/destination/[city_id]/bureaucracy/page.js
+++ src/app/destination/[city_id]/bureaucracy/page.js
@@ -8,24 +8,12 @@ import {
   Box,
   Tabs,
   Tab,
-  Stepper,
-  Step,
-  StepLabel,
   Chip,
   Card,
   CardContent,
   CardActions,
   Button,
   Grid,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   Tooltip,
   IconButton,
 } from "@mui/material";
@@ -35,6 +23,7 @@ import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import SendGAPageView from "components/DataLoaders/SendGAPageView";
 import RequirementsModal from "components/Modals/RequirementsModal";
 import BureaucracyAccordeon from "components/Accordions/bureaucracyAccordeon";
+import EntreTramitesExp from "components/Cards/EntreTramitesExp";
 
 import { imagesCdn } from "constants/constants";
 
@@ -200,92 +189,10 @@ const BureaucracyPage = () => {
             width: "100%",
           }}
         >
-          <Card
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              textAlign: "center",
-              my: 1,
-              padding: 2,
-              width: "95%",
-            }}
-          >
-            <Box>
-              <Typography variant="h3" gutterBottom sx={{ mb: 2 }}>
-                Essentials
-              </Typography>
-              <Chip
-                label={
-                  <div>
-                    Discount code: <b>10SITYEX</b>
-                  </div>
-                }
-                sx={{ backgroundColor: "#673ab7", color: "white" }}
-              />
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ fontSize: 12, my: 1 }}
-              >
-                **Remember to apply the code when you are in the payment page**
-              </Typography>
-              <img
-                width="200"
-                height="50"
-                src={`${imagesCdn}/partner_logos/entretramites.webp`}
-                alt="entretramites icon"
-                title="Entre Tramites"
-                style={{ marginTop: 15 }}
-              />
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ fontSize: 16, m: 2 }}
-              >
-                We have partnered with <b>Entre Trámites</b> to offer you a{" "}
-                <b>10% discount</b> on all their essential services. They will
-                take<b> care of the whole process for you, 100% online.</b>
-              </Typography>
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ fontSize: 16, m: 2 }}
-              >
-                Still not sure what you need?
-              </Typography>
-              <Button
-                sx={{ alignSelf: "center" }}
-                size="small"
-                variant="contained"
-                href="https://entretramites.com/en/partners-program/free-consultation-partners?aff=3d90441f"
-                target="_blank"
-              >
-                Get free consultation
-              </Button>
-              <Box sx={{ mt: 3 }}>
-                <Stepper alternativeLabel>
-                  <Step key="first-step">
-                    <StepLabel>First step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Choose the documents you need
-                    </Typography>
-                  </Step>
-                  <Step key="second-step">
-                    <StepLabel>Second step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Redeem your discount through our code
-                    </Typography>
-                  </Step>
-                  <Step key="thid-step">
-                    <StepLabel>Third step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Wait from an email from Entre Trámites
-                    </Typography>
-                  </Step>
-                </Stepper>
-              </Box>
-            </Box>
-          </Card>
+          <EntreTramitesExp
+            ServiceName="Essentials"
+            showFreeConsultation={true}
+          />
           <Typography variant="h3" gutterBottom sx={{ m: 2 }}>
             Available services
           </Typography>
@@ -835,76 +742,7 @@ const BureaucracyPage = () => {
             alignItems: "center",
           }}
         >
-          <Card
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              textAlign: "center",
-              my: 1,
-              padding: 2,
-              width: "95%",
-            }}
-          >
-            <Box>
-              <Typography variant="h3" gutterBottom sx={{ mb: 2 }}>
-                How to get a Visa
-              </Typography>
-              <Chip
-                label={
-                  <div>
-                    Discount code: <b>10SITYEX</b>
-                  </div>
-                }
-                sx={{ backgroundColor: "#673ab7", color: "white" }}
-              />
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ fontSize: 12, my: 1 }}
-              >
-                **Remember to apply the code when you are in the payment page**
-              </Typography>
-              <img
-                width="200"
-                height="50"
-                src={`${imagesCdn}/partner_logos/entretramites.webp`}
-                alt="entretramites icon"
-                title="Entre Tramites"
-                style={{ marginTop: 15 }}
-              />
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ fontSize: 16, m: 2 }}
-              >
-                We have partnered with <b>Entre Trámites</b> to offer you a{" "}
-                <b>10% discount</b> on all their Visa services. They will take
-                <b> care of the whole process for you, 100% online.</b>
-              </Typography>
-              <Box sx={{ mt: 3 }}>
-                <Stepper alternativeLabel>
-                  <Step key="first-step">
-                    <StepLabel>First step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Choose the documents you need
-                    </Typography>
-                  </Step>
-                  <Step key="second-step">
-                    <StepLabel>Second step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Redeem your discount through our code
-                    </Typography>
-                  </Step>
-                  <Step key="thid-step">
-                    <StepLabel>Third step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Wait for an email from Entre Trámites
-                    </Typography>
-                  </Step>
-                </Stepper>
-              </Box>
-            </Box>
-          </Card>
+          <EntreTramitesExp ServiceName="Visa" showFreeConsultation={false} />
           <Typography variant="h3" gutterBottom sx={{ m: 2 }}>
             Available Visas
           </Typography>
@@ -1378,77 +1216,10 @@ const BureaucracyPage = () => {
             width: "100%",
           }}
         >
-          <Card
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              textAlign: "center",
-              my: 1,
-              padding: 2,
-              width: "95%",
-            }}
-          >
-            <Box>
-              <Typography variant="h3" gutterBottom sx={{ mb: 2 }}>
-                Driver's And Vehicle Licensing
-              </Typography>
-              <Chip
-                label={
-                  <div>
-                    Discount code: <b>10SITYEX</b>
-                  </div>
-                }
-                sx={{ backgroundColor: "#673ab7", color: "white" }}
-              />
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ fontSize: 12, my: 1 }}
-              >
-                **Remember to apply the code when you are in the payment page**
-              </Typography>
-              <img
-                width="200"
-                height="50"
-                src={`${imagesCdn}/partner_logos/entretramites.webp`}
-                alt="entretramites icon"
-                title="Entre Tramites"
-                style={{ marginTop: 15 }}
-              />
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ fontSize: 16, m: 2 }}
-              >
-                We have partnered with <b>Entre Trámites</b> to offer you a{" "}
-                <b>10% discount</b> on all their Driver's and Vehicle Licensing.
-                They will take
-                <b> care of the whole process for you, 100% online.</b>
-              </Typography>
-              <Box sx={{ mt: 3 }}>
-                <Stepper alternativeLabel>
-                  <Step key="first-step">
-                    <StepLabel>First step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Choose the documents you need
-                    </Typography>
-                  </Step>
-                  <Step key="second-step">
-                    <StepLabel>Second step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Redeem your discount through our code
-                    </Typography>
-                  </Step>
-                  <Step key="thid-step">
-                    <StepLabel>Third step</StepLabel>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      Wait for an email from Entre Trámites
-                    </Typography>
-                  </Step>
-                </Stepper>
-              </Box>
-            </Box>
-          </Card>
+          <EntreTramitesExp
+            ServiceName="Driver's And Vehicle Licensing"
+            showFreeConsultation={false}
+          />
           <Typography variant="h3" gutterBottom sx={{ m: 2 }}>
             Available services
           </Typography>
---
+++ src/components/Cards/EntreTramitesExp.jsx
@@ -0,0 +1,113 @@
+"use client";
+
+import React from "react";
+
+import {
+  Typography,
+  Box,
+  Stepper,
+  Step,
+  StepLabel,
+  Chip,
+  Card,
+  Button,
+} from "@mui/material";
+
+import { imagesCdn } from "constants/constants";
+
+
+const EntreTramitesExp = ({ ServiceName, showFreeConsultation }) => {
+
+
+    return (<Card
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+          my: 1,
+          padding: 2,
+          width: "95%",
+        }}
+      >
+        <Box>
+          <Typography variant="h3" gutterBottom sx={{ mb: 2 }}>
+            {ServiceName}
+          </Typography>
+          <Chip
+            label={
+              <div>
+                Discount code: <b>10SITYEX</b>
+              </div>
+            }
+            sx={{ backgroundColor: "#673ab7", color: "white" }}
+          />
+          <Typography
+            variant="body1"
+            gutterBottom
+            sx={{ fontSize: 12, my: 1 }}
+          >
+            **Remember to apply the code when you are in the payment page**
+          </Typography>
+          <img
+            width="200"
+            height="50"
+            src={`${imagesCdn}/partner_logos/entretramites.webp`}
+            alt="entretramites icon"
+            title="Entre Tramites"
+            style={{ marginTop: 15 }}
+          />
+          <Typography
+            variant="body1"
+            gutterBottom
+            sx={{ fontSize: 16, m: 2 }}
+          >
+            We have partnered with <b>Entre Trámites</b> to offer you a{" "}
+            <b>10% discount</b> on all their {ServiceName} services. They will
+            take<b> care of the whole process for you, 100% online.</b>
+          </Typography>
+          {showFreeConsultation && (
+          <Box>
+          <Typography
+            variant="body1"
+            gutterBottom
+            sx={{ fontSize: 16, m: 2 }}
+          >
+            Still not sure what you need?
+          </Typography>
+          <Button
+            sx={{ alignSelf: "center" }}
+            size="small"
+            variant="contained"
+            href="https://entretramites.com/en/partners-program/free-consultation-partners?aff=3d90441f"
+            target="_blank"
+          >
+            Get free consultation
+          </Button>
+          </Box>)}
+          <Box sx={{ mt: 3 }}>
+            <Stepper alternativeLabel>
+              <Step key="first-step">
+                <StepLabel>First step</StepLabel>
+                <Typography variant="body1" sx={{ mt: 1 }}>
+                  Choose the documents you need
+                </Typography>
+              </Step>
+              <Step key="second-step">
+                <StepLabel>Second step</StepLabel>
+                <Typography variant="body1" sx={{ mt: 1 }}>
+                  Redeem your discount through our code
+                </Typography>
+              </Step>
+              <Step key="thid-step">
+                <StepLabel>Third step</StepLabel>
+                <Typography variant="body1" sx={{ mt: 1 }}>
+                  Wait from an email from Entre Trámites
+                </Typography>
+              </Step>
+            </Stepper>
+          </Box>
+        </Box>
+      </Card>);
+}
+
+export default EntreTramitesExp;
