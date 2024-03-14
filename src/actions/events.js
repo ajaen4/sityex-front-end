0fcda962408f4937e217888d36ff3db29ac2a052
ajaen4 <a.jaenrev@gmail.com>
2024-03-14 10:56:45 +0100

    Delete events section and substitute it with community

--- src/actions/events.js
+++ src/actions/events.js
@@ -4,7 +4,6 @@ import {
   isFetchingEvents,
   fetchingEventsSuccess,
   fetchingEventSuccess,
-  setEventsOrderBy,
 } from "store/reducers/events";
 
 export const getCityEvents = (city_id) => (dispatch, getState) => {
@@ -31,25 +30,4 @@ export const getCityEvent = (city_id, event_id) => (dispatch, getState) => {
   api.getCityEvent(city_id, event_id).then((selected_event) => {
     dispatch(fetchingEventSuccess({ selected_event }));
   });
-};
-
-export const setUserInterested = (
-  city_id,
-  event_id,
-  user_id,
-  interested_info,
-) => {
-  api.setUserInterested(city_id, event_id, user_id, interested_info);
-};
-
-export const countInterestedUsers = (city_id, event_id, user_id) => {
-  return api.countInterestedUsers(city_id, event_id, user_id);
-};
-
-export const updateEventsOrderBy = (orderBy) => (dispatch, _) => {
-  dispatch(
-    setEventsOrderBy({
-      orderBy: orderBy,
-    }),
-  );
 };
--- src/store/reducers/events.js
+++ src/store/reducers/events.js
@@ -1,14 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { filterEvents, getCategories, groupEvents } from "helpers/listUtils.js";
 
 const eventsSlice = createSlice({
   name: "events",
   initialState: {
     data: {
-      groupedEvent: [],
-      categories: [],
+      events: [],
       selectedEvent: null,
-      orderBy: "",
     },
     city_id: null,
     isFetching: false,
@@ -18,32 +15,15 @@ const eventsSlice = createSlice({
       state.isFetching = true;
     },
     fetchingEventsSuccess: (state, action) => {
-      const categories = getCategories(action.payload.events);
-      state.data.categories = categories;
-      const groupedEvents = groupEvents(categories, action.payload.events);
-      state.data.groupedEvents = groupedEvents;
-      state.city_id = action.payload.city_id;
-      state.data.selectedEvent = null;
+      state.data.events = action.payload.events;
     },
     fetchingEventSuccess: (state, action) => {
       state.data.selectedEvent = action.payload.selected_event;
     },
-    setEventsOrderBy: (state, action) => {
-      const orderBy = action.payload.orderBy;
-      state.data.orderBy = orderBy;
-      state.data.filteredGEvents = filterEvents(
-        state.data.groupedEvents,
-        orderBy,
-      );
-    },
   },
 });
 
-export const {
-  isFetchingEvents,
-  fetchingEventsSuccess,
-  fetchingEventSuccess,
-  setEventsOrderBy,
-} = eventsSlice.actions;
+export const { isFetchingEvents, fetchingEventsSuccess, fetchingEventSuccess } =
+  eventsSlice.actions;
 
 export default eventsSlice.reducer;
